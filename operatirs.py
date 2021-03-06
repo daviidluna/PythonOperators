c88a626679f17cd6f8b3dac89899d56ff027ab58
David Luna <davidluna@mail.weber.edu>
2021-03-06 11:07:48 -0700

    added some operators

--- operatirs.py
+++ operatirs.py
@@ -6,5 +6,19 @@ other_ = '{:,}'.format(203482309+238423482343)
 print(other_)
 division = '{:,}'.format(8100000000/9)
 print(division)
-exponentiation = '{:,}'.format(384**585890)
-print(exponentiation)
+
+x = 6
+x *= 9
+print(x)
+x = 78
+y = 87
+print(x == y)
+x = 9 
+y = 980980980
+print(x != y)
+x = 67
+print (x > 5 and x > 54)
+x = 80
+print(x > 87 or x < 87)
+x = 8 
+print(not(x > 4 and x < 9))
